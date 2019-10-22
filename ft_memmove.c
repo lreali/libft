3c5f73c38effe28e32bbbc82bece5136d9eb78bb
Elsa REALI <ereali@e2r7p22.42.fr>
2019-10-22 23:42:07 +0200

    memmovestatus

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ereali <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 23:15:59 by ereali            #+#    #+#             */
-/*   Updated: 2019/10/22 18:27:30 by ereali           ###   ########.fr       */
+/*   Updated: 2019/10/22 23:17:42 by ereali           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,26 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	unsigned char	buffer[len];
-	unsigned int	i;
+	size_t				i;
+	unsigned char		*s1;
+	const unsigned char	*s2;
 
+	s1 = (unsigned char*)dst;
+	s2 = (unsigned char*)src;
 	i = 0;
-	if ((!(dst) && len == 0) || (!(dst) && !(src)))
+	if (!dst && !src)
 		return (NULL);
-	while (i < len)
-	{
-		buffer[i] = ((const char *)src)[i];
-		i++;
-	}
-	i = 0;
-	while (i < len)
-	{
-		((unsigned char *)dst)[i] = buffer[i];
-		i++;
-	}
+	if (s2 < s1)
+		while (i < len)
+		{
+			s1[len - i - 1] = s2[len - i - 1];
+			i++;
+		}
+	else
+		while (len > 0)
+		{
+			*(s1++) = *(s2++);
+			len--;
+		}
 	return (dst);
 }
