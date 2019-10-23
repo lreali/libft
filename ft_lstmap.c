d26a84d8e24cc22231dd926bf1bfb82e7bc8986a
Elsa REALI <ereali@e2r9p14.42.fr>
2019-10-23 16:26:20 +0200

    ma

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: ereali <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 18:02:25 by ereali            #+#    #+#             */
-/*   Updated: 2019/10/23 01:15:33 by ereali           ###   ########.fr       */
+/*   Updated: 2019/10/23 16:25:18 by ereali           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,21 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*firstelement;
 	t_list	*newlist;
 
-	if (!lst || !(f))
+	if (!lst || !f)
 		return (NULL);
 	newlist = ft_lstnew(f(lst->content));
 	firstelement = newlist;
-	while (lst)
+	while (lst->next)
 	{
 		lst = lst->next;
+		newlist = newlist->next;
 		if (!(newlist = f(lst->content)))
 			{
 				del(newlist->content);
 				free(newlist);
 				return (NULL);
 			}
-		newlist = newlist->next;
 	}
+	newlist->next = NULL;
 	return (firstelement);
 }
